b89d0ad9b83725943f8f2f56d432c32f9c47cf40
Spencer Holladay <spencer.holladay@gmail.com>
2019-11-27 20:11:53 -0500

    deletes old files

--- Test Files/script.js
+++
@@ -1,8 +0,0 @@
-function show() {
-  var x = document.getElementById('navbar');
-  if (x.style.display === 'block') {
-    x.style.display = 'none';
-  } else {
-    x.style.display = 'block';
-  }
- }
--- script.js
+++
@@ -1,5 +0,0 @@
-$(window).load(function () {
-  $('.nav_slide_button').click(function () {
-    $('.pull').slideToggle();
-  });
-});
