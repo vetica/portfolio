9014bfef7f8df7441e71a49c89baf624ff5e1aa1
Spencer Holladay <spencer.holladay@gmail.com>
2019-11-27 20:11:42 -0500

    adds new baseline files

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Opens Navbar
+function show() {
+  var x = document.getElementById('navbar');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+ }
