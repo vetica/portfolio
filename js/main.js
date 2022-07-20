ad029fb4d1cd96d1204adad3510d0fb289611de1
vetica <sholladay@gannett.com>
2022-07-20 11:10:27 -0400

    adds new base files

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// Opens Navbar
-function show() {
-  var x = document.getElementById('navbar');
-  if (x.style.display === 'block') {
-    x.style.display = 'none';
-  } else {
-    x.style.display = 'block';
-  }
- }
